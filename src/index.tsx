b6f74a0e95f4507312d51e99a5ae275fe3d0a39b
Tyler Long <tyler4long@gmail.com>
2021-09-13 11:09:36 -0700

    Hello fastify

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDomServer from 'react-dom/server';
+import Fastify from 'fastify';
 
 const element = <h1>Hello</h1>;
 
@@ -7,3 +8,16 @@ const html = ReactDomServer.renderToString(element);
 console.log(html);
 const html2 = ReactDomServer.renderToStaticMarkup(element);
 console.log(html2);
+
+const fastify = Fastify();
+
+fastify.get('/', (request, reply) => {
+  reply.type('text/html').send(html);
+});
+
+fastify.listen(8080, '127.0.0.1', (error, address) => {
+  if (error) {
+    throw error;
+  }
+  console.log(address);
+});
